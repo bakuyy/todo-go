e7ceeb54028b3a028c8744363842a72982aefb24
bakuyy <sophieyang12345@gmail.com>
2023-07-09 22:29:55 -0400

    final changes

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,35 +1,61 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function TodoForm() {
-    const [Text, setText] = useState('')
-    const [storedText, setStoredText] = useState('')
+  const [text, setText] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-    function HandleChange(event) {
-        const newVal = event.target.value
-        setText(newVal)
+  useEffect(() => {
+    getTasks()
+  }, []);
+
+  const handleChange = (event) => {
+    const newVal = event.target.value;
+    setText(newVal)
+  };
+
+  const handleSubmitButton = async () => {
+    try {
+      await axios.post('http://localhost:8080/tasks', { task: text });
+      setText('');
+      getTasks();
+    } catch (error) {
+      console.log('Error adding task:', error);
     }
+  };
 
-    const HandleSubmitButton = async ()=> {
-      try {
-        await axios.post('http://localhost:8080/tasks',{ task })
-        setText('')
-      } catch (error) {
-        console.log("error adding task", error)
-      }
-
+  const getTasks = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/tasks');
+      setTasks(response.data);
+    } catch (error) {
+      console.log('Error loading tasks:', error);
     }
+  };
 
   return (
     <div>
-    <div className='flex mt-3 '>
-    <input className='text-sm p-2 border-2 border-gray-400 rounded-xl' value={Text} onChange={HandleChange} placeholder='Add item...'></input>
-    <button className='ml-3 bg-gray-200 p-2 rounded-xl' onClick={HandleSubmitButton}> Submit</button>
-  
-  </div>
+      <div className="flex mt-3 mb-12">
+        <input
+          className="text-sm p-2 border-2 border-gray-400 rounded-xl ml-96 "
+          value={text}
+          onChange={handleChange}
+          placeholder="Add item to do :) ..."
+        />
+        <button
+          className="ml-3 bg-gray-200 p-2 rounded-xl"
+          onClick={handleSubmitButton}
+        >
+          Submit
+        </button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li className='bg-red-400 p-4 ml-96 mr-96 rounded-xl my-2' key={task.id}>{task.task}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default TodoForm
+export default TodoForm;
